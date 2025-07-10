9fba381bd315e696a958a0ad13422c15782ddc9f
Aerin Jang <205189969+Xerin-J@users.noreply.github.com>
2025-07-10 21:02:02 +0900

    Yelp done

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :restaurants, only: %i[index new create show] do
+    resources :reviews, only: %i[create]
+  end
+  root to: "restaurants#index"
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,37 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.create!(
+  [
+    {
+      name: "Sushiro",
+      address: "Hamamatsu Chuo-ku",
+      phone_number: "0501234567",
+      category: "japanese"
+    },
+    {
+      name: "Happy Valley",
+      address: "Hamamatsu Nishiiba",
+      phone_number: "0505678901",
+      category: "italian"
+    },
+    {
+      name: "Rorin",
+      address: "Wakabayashi",
+      phone_number: "0506661234",
+      category: "chinese"
+    },
+    {
+      name: "Maison Michi",
+      address: "Shizuoka",
+      phone_number: "06099887766",
+      category: "french"
+    },
+    {
+      name: "Antwerp Central",
+      address: "Tokyo Chiyoda-ku ",
+      phone_number: "03044556677",
+      category: "belgian"
+    }
+  ]
+)
